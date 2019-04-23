1a4e73a0206123a893ea73258dfcb71be496ed30
Nick TenBrink <tenbrinknicholas@gmail.com>
2019-04-23 10:39:01 -0400

    initial commit

---
+++ src/perimeter.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class perimeter {
+
+	public static void main(String[] args) {
+
+		// boolean bool = true;
+		String answer = "";
+		do {
+			System.out.println("enter the length: ");
+			Scanner scnr = new Scanner(System.in);
+			double len = scnr.nextDouble();
+			System.out.println("enter the width: ");
+			double width = scnr.nextDouble();
+
+			double area = len * width;
+			double perim = 2 * len + 2 * width;
+
+			double volume = len * width * 9;
+
+			System.out.println("area: " + area);
+			System.out.println("perimeter: " + perim);
+			System.out.println("volume: " + volume);
+
+			scnr.nextLine(); // garbage collection
+
+			System.out.println("do you want to calculate another room? (y/n)");
+			answer = scnr.nextLine();
+			// scnr.nextLine();
+			// scnr.close();
+		} while (answer.equalsIgnoreCase("y"));
+		System.out.println("Goodbye");
+
+	}
+
+}
